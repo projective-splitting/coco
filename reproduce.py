e01a7af3f0eb3e3fc2be3ef009b1b38046f8e8b4
Jonathan Eckstein <jeckstei@business.rutgers.edu>
2020-02-21 21:01:24 -0500

    Cleaned up some printouts and moved run length warning earlier

--- reproduce.py
+++ reproduce.py
@@ -9,22 +9,26 @@ if sys.version_info[0]==2:
 else:
     which2run = input("Enter which problem to solve: portfolio, group_lr, or rare_feature >>> ")
 
-print('you selected '+ which2run)
-if which2run=='portfolio':
-    options = '1,2,3,4'
-elif which2run=='group_lr':
-    options = '1,2,3,4,5,6'
-elif which2run=='rare_feature':
-    options = '1,2,3'
-else:
-    print('did not enter a correct problem, exiting')
-    exit()
-whichExp = int(input("Enter which experiment to run from {"+(options)+"} >>> "))
-print('you selected '+str(whichExp))
-
+print('You selected '+ which2run)
 if which2run=='portfolio':
     print('WARNING: running time on our machine for this experiment was 2.5 minutes,')
-    print('may be longer for you.')
+    print('and it may be longer for you.')
+    options = '1,2,3,4'
+elif which2run=='group_lr':
+    print('WARNING: running time on our machine for this experiment was 5 minutes,')
+    print('and it may be longer for you.')
+    options = '1,2,3,4,5,6'
+elif which2run=='rare_feature':
+    print('WARNING: running time on our machine for this experiment was 30 minutes,')
+    print('and it may be longer for you.')
+    options = '1,2,3'
+else:
+    print('You did not enter a correct problem, exiting')
+    exit()
+whichExp = int(input("Enter which experiment to run from {"+(options)+"} >>> "))
+print('You selected '+str(whichExp))
+
+if which2run=='portfolio':
     if whichExp==1:
         os.system(' python run_portfolio.py --dimension 10000')
     elif whichExp==2:
@@ -36,10 +40,8 @@ if which2run=='portfolio':
         os.system('python run_portfolio.py --dimension 10000 --deltar 1.5 --gamma1f 5 --gamma2f 10 \
                                 --betacp 0.5 --gammafrb 10 --gammatg 10')
     else:
-        print('Did not enter an experiment number of 1,2,3, or 4. (four options for portfolio)')
+        print('You did not enter an experiment number of 1,2,3, or 4.  Exiting.')
 elif which2run=='group_lr':
-    print('WARNING: running time on our machine for this experiment was 5 minutes,')
-    print('may be longer for you.')
     if whichExp==1:
         os.system('python run_group_lr.py --lam1 0.05 --lam2 0.05 --dataset breastCancer \
                                --gamma1f 0.05 --iter 3000')
@@ -60,10 +62,8 @@ elif which2run=='group_lr':
         os.system('python run_group_lr.py --lam1 1.0 --lam2 1.0 --gammatg 1e6 --gammafrb 1e6\
                                --betacp 1e-5 --iter 3000')
     else:
-        print('did not enter an experiment number of 1,2,3,4,5,6. (six options for group_lr)')
+        print('You did not enter an experiment number of 1,2,3,4,5,6.  Exiting.')
 elif which2run=='rare_feature':
-    print('WARNING: running time on our machine for this experiment was 30 minutes,')
-    print('may be longer for you.')
     if whichExp==1:
         os.system('python run_rare_feature.py --gamma1f 1.0 --iter 10000')
     elif whichExp==2:
@@ -73,4 +73,4 @@ elif which2run=='rare_feature':
         os.system('python run_rare_feature.py --lam 1e-1 --gamma1f 1e4 --gamma2f 1e5 \
                                    --betacp 1e-7 --gammatg 1e6 --gammafrb 1e6 --iter 10000')
     else:
-        print('did not enter an experiment number of 1,2,3 (three options for rare_feature)')
+        print('You did not enter an experiment number of 1,2,3.  Exiting.')
